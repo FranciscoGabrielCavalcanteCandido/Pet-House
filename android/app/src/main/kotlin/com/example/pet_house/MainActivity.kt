1464b536c78865550875f536aea73fdb26491536
FranciscoGabrielCavalcanteCandido <fg.14.cavalcandido@gmail.com>
2022-09-27 22:34:50 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/pet_house/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pet_house
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(60),
+      decoration: const BoxDecoration(color: Colors.white),
+      child: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
